0ed4846d68c8763592d6142b516becef23fa9084
idwxy <idwxy@live.com>
2019-10-17 17:09:47 +0800

    负责用户登录注册的 mapper 接口

---
+++ src/main/java/com/idwxy/blogdemo/mapper/UserMapper.java
@@ -0,0 +1,33 @@
+package com.idwxy.blogdemo.mapper;
+
+import com.idwxy.blogdemo.entity.UserEntity;
+import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.type.JdbcType;
+import org.springframework.stereotype.Repository;
+
+// 声明 MyBatis 的 Mapper
+@Mapper
+// Spring Boot 的注解，是这个文件能够被扫描到
+@Repository
+public interface UserMapper {
+
+    @Insert("INSERT INTO 'user' ('username', 'password', 'register_time')" +
+            "VALUES (#{userName}, #{password}, #{registerTime})")
+    int createUser(@Param("userName") String userName, @Param("password") String password,
+                   @Param("registerTime") Long registerTime);
+
+    @Results(id = "userMap", value = {
+            @Result(property = "uid", column = "uid"),
+            @Result(property = "userName", column = "username"),
+            @Result(property = "password", column = "password"),
+            @Result(property = "role", column = "role"),
+            @Result(property = "registerTime", column = "register_time", jdbcType = JdbcType.BIGINT)
+    })
+    @Select("SELECT * FROM 'user' WHERE 'username' = #{userName} AND 'password' = #{password} ")
+    UserEntity findByUserNameAndPassword(@Param("userName") String userName,
+                                         @Param("password") String password);
+
+    @Select("SELECT * FROM 'user' WHERE 'username' = #{userName}")
+    @ResultMap("userMap")
+    UserEntity findByUserName(@Param("userName") String userName);
+}
