413fbee5443421de3a2611e8fa4d1d40c63cadb8
idwxy <idwxy@live.com>
2019-10-19 17:36:17 +0800

    加 @EnableSwagger2 注解即可开启 Swagger

--- src/main/java/com/idwxy/blogdemo/BlogdemoApplication.java
+++ src/main/java/com/idwxy/blogdemo/BlogdemoApplication.java
@@ -3,9 +3,11 @@ package com.idwxy.blogdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableCaching
+@EnableSwagger2
 public class BlogdemoApplication {
 
 	public static void main(String[] args) {
