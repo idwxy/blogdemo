a1c211a621df7ded98f7eee8632845be059570e9
idwxy <idwxy@live.com>
2019-10-19 17:34:19 +0800

    完成 Spring Security 中与数据库的交互环节

--- src/main/java/com/idwxy/blogdemo/security/UserDetailsServiceImpl.java
+++ src/main/java/com/idwxy/blogdemo/security/UserDetailsServiceImpl.java
@@ -1,5 +1,11 @@
 package com.idwxy.blogdemo.security;
 
+import com.idwxy.blogdemo.entity.UserEntity;
+import com.idwxy.blogdemo.mapper.UserMapper;
+import com.idwxy.blogdemo.util.Util;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +13,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        return null;
+        UserEntity userEntity = userMapper.findByUserName(userName);
+        if (userEntity == null) {
+            throw new UsernameNotFoundException("该用户不存在");
+        }
+        // 存储当前线程的 uid 到 ThreadLocal 中
+        Util.setCurrentUid(userEntity.getUid());
+        return new User(userEntity.getUserName(), userEntity.getPassword(),
+                AuthorityUtils.createAuthorityList(userEntity.getRole()));
     }
 }
