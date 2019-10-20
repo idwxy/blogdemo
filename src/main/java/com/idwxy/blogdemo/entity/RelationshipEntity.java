fa23c2622bd525dada3b267561764f4c8f9f9684
idwxy <idwxy@live.com>
2019-10-20 18:31:27 +0800

    创建关系实体类

---
+++ src/main/java/com/idwxy/blogdemo/entity/RelationshipEntity.java
@@ -0,0 +1,15 @@
+package com.idwxy.blogdemo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RelationshipEntity {
+
+    private Integer tid;
+
+    private Integer cid;
+}
