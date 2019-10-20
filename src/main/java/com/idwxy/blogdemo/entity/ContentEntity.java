0ec32b9fd017da25bccc55598aeccafdcf7a00b6
idwxy <idwxy@live.com>
2019-10-20 18:28:56 +0800

    创建文章实体类

---
+++ src/main/java/com/idwxy/blogdemo/entity/ContentEntity.java
@@ -0,0 +1,21 @@
+package com.idwxy.blogdemo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ContentEntity {
+
+    private Integer cid;
+
+    private String title;
+
+    private String content;
+
+    private Integer authorId;
+
+    private Long createTime;
+}
