815aaf17abf9214038dc075fc3d00d2a25752c2b
idwxy <idwxy@live.com>
2019-10-28 19:40:23 +0800

    新建文章控制器，完成创建文章业务

---
+++ src/main/java/com/idwxy/blogdemo/controller/ContentController.java
@@ -0,0 +1,44 @@
+package com.idwxy.blogdemo.controller;
+
+import com.idwxy.blogdemo.security.IsUser;
+import com.idwxy.blogdemo.service.ContentService;
+import com.idwxy.blogdemo.service.TagsService;
+import com.idwxy.blogdemo.util.Result;
+import com.idwxy.blogdemo.util.Util;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/content")
+@Validated
+public class ContentController {
+
+    @Autowired
+    private ContentService contentService;
+
+    @Autowired
+    private TagsService tagsService;
+
+    @ApiOperation(value = "创建文章", notes = "创建文章，创建成功返回文章id")
+    @ApiImplicitParam(name = "tags", value = "文章标签，使用,号分隔", required = true)
+    @IsUser
+    @PostMapping
+    public Result createContent(@RequestParam @Length(min = 1,max = 100) String title,
+                                @RequestParam @Length(min = 1) String content,
+                                @RequestParam @Length(min = 1) String tags) {
+        String[] tagsSpliced = tags.split(",");
+        if (tagsSpliced.length == 0) {
+            return new Result(HttpStatus.BAD_REQUEST.value(), "请至少输入一个标签", null);
+        }
+        int cid = contentService.createContent(title, content, tagsSpliced, Util.getCurrentUid());
+        return new Result(HttpStatus.OK.value(), "文章创建成功", cid);
+    }
+}
